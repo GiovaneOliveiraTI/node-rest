a2edc751adac482e2789d4e029906bbeec033d0e
f550971 <f550971@corp.caixa.gov.br>
2021-02-21 13:35:03 -0300

    Validandos dados.

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,19 +1,45 @@
 const conexao = require('../infra/conexao')
 const moment = require('moment')
 class Atendimento {
-    adiciona(atendimento) {
+    adiciona(atendimento, res) {
         const datacriacao = moment().format('YYYY-MM-DD HH:MM:SS')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-        const atendimentoDatado = {...atendimento, datacriacao, data }
-        const sql = 'INSERT INTO Atendimentos SET ? '
+        const dataEhValida = moment(data).isSameOrAfter(datacriacao)
+        const clienteEhValido = atendimento.cliente.length >= 5
 
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-            if (erro) {
-                console.log(erro)
-            } else {
-                console.log(resultados)
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos cinco carcteres.'
             }
-        })
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+
+        if(existemErros) {
+            res.status(400).json(erros)
+        } else {
+            const atendimentoDatado = {...atendimento, datacriacao, data }
+            const sql = 'INSERT INTO Atendimentos SET ? '
+
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if (erro) {
+                    res.status(400).json(erro)
+                } else {
+                    res.status(201).json(resultados)
+                }
+            })
+
+        }
+
     }
 }
 
