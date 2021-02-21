d035ed14eabd22c9d9fec6b7a047689183de9db3
f550971 <f550971@corp.caixa.gov.br>
2021-02-21 13:06:35 -0300

    salvando data de criação no banco.

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,10 +1,13 @@
 const conexao = require('../infra/conexao')
-
+const moment = require('moment')
 class Atendimento {
     adiciona(atendimento) {
+        const datacriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        const atendimentoDatado = {...atendimento, datacriacao, data }
         const sql = 'INSERT INTO Atendimentos SET ? '
 
-        conexao.query(sql, atendimento, (erro, resultados) => {
+        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
             if (erro) {
                 console.log(erro)
             } else {
