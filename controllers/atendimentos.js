f795aa2495573809465f5e6d9814fa174195eafb
f550971 <f550971@corp.caixa.gov.br>
2021-02-21 12:35:34 -0300

    criando controller para fazer insert.

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,9 +1,13 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
     app.get('/atendimentos', (req, res)=> res.send('Você esta na rota de atendimentos e esta rodando um get'))
 
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body)
-        res.send('você vesta na roda atendimento e esta rodando um POST')
+        const atendimento = req.body
+
+        Atendimento.adiciona(atendimento)
+        res.send('post atendimento')
     })
 }
 
--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const customExpress = require('./config/customExpress')
 
-const app = customExpress()
 
-app.listen(3000, ()=> console.log('Sevidor rodando na porta 3000'))
+const conexao = require('./infra/conexao')
+const tabelas = require('./infra/tabelas')
+
+conexao.connect(erro =>  {
+    if (erro) {
+        console.log(erro);
+
+    } else {
+        console.log('conectado com sucesso!')
+
+        tabelas.init(conexao)
+        const app = customExpress()
+
+
+        app.listen(3000, ()=> console.log('Sevidor rodando na porta 3000'))
+    }
+})
+
 
 
