40f3b21504a712333f299bf19971db3a20e415a0
f550971 <f550971@corp.caixa.gov.br>
2021-02-22 23:13:38 -0300

    fazendo buscar todos e buscar por Id.

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,7 +1,16 @@
 const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
-    app.get('/atendimentos', (req, res)=> res.send('Você esta na rota de atendimentos e esta rodando um get'))
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.listaAtendimentos(res)
+    })
+
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+
+        Atendimento.buscaPorId(id, res)
+
+    })
 
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body
